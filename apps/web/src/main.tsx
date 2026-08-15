9a8ed32f674f7e2911fd8e2f90ad77da6dd96f1e
Stanimir Ivanov <stanimir.ivanov@gmail.com>
2026-08-15 17:49:36 +0300

    [#11] Show vehicles on map (#12)
    Co-authored-by: Stanimir Ivanov <stanimr@protonmail.com>

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,15 +2,16 @@ import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
+import AppGL from './app/AppGL';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AppGL /> {/* Render AppGL instead of App */}
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
